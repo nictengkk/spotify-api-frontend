4a5321b014e629f749fd4890d4f4ebe89eed4c5f
nictengkk <nictengkk@gmail.com>
2019-05-07 19:47:43 +0800

    update SongAnalysis page to render information on song clicked

--- src/Pages/SongAnalysis/SongAnalysis.js
+++ src/Pages/SongAnalysis/SongAnalysis.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
-import SearchInput from "../../components/SearchInput/SearchInput";
+// import SearchInput from "../../components/SearchInput/SearchInput";
 
 export class SongAnalysis extends Component {
   constructor(props) {
     super(props);
     this.state = {
       id: "",
-      token: ""
+      token: "",
+      albumImgUrl: "",
+      result: {}
     };
   }
 
   async componentDidMount() {
     try {
-      const { token } = this.props.location.state;
-      const id = "2CIMQHirSU0MQqyYHq0eOx";
+      const { id, token, albumImgUrl, albumName } = this.props.location.state;
+      console.log(id);
+      console.log(token);
+      // console.log(albumImgUrls);
+      // const id = "2CIMQHirSU0MQqyYHq0eOx";
       const res = await fetch(
         `https://api.spotify.com/v1/audio-analysis/${id}`,
         {
@@ -25,17 +30,40 @@ export class SongAnalysis extends Component {
       );
       const result = await res.json();
       console.log(result);
-      this.setState({ token });
+      this.setState({
+        albumImgUrl,
+        token,
+        id,
+        result: result,
+        albumName: albumName
+      });
     } catch (error) {
       console.error(error);
     }
   }
 
   render() {
+    const { id, trackName, albumImgUrl, albumName } = this.props.location.state;
+    // console.log(albumImgUrls);
     return (
-      <div>
-        <div>Song Analysis</div>
-        <SearchInput />
+      <div className="container fluid">
+        <div className="row justify-content-center">
+          <h1>Track Analysis</h1>
+        </div>
+        <div className="row justify-content-center">
+          <p>Track Name: {trackName} </p>
+        </div>
+        <div className="row justify-content-center">
+          <p>Track ID: {id}</p>
+        </div>
+        <div className="row justify-content-center">
+          <p>Album Name: {albumName} </p>
+        </div>
+        <div className="row justify-content-center">
+          <p>
+            {<img src={albumImgUrl} alt="albumImg" width={200} height={200} />}{" "}
+          </p>
+        </div>
       </div>
     );
   }
