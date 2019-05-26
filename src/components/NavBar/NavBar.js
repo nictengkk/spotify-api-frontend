d5c3ba0c15af5488ef454502116782bf02445789
nictengkk <nictengkk@gmail.com>
2019-05-27 02:06:47 +0800

    create NavBar to navigate between pages

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Nav, NavItem, NavLink } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+class NavBar extends React.Component {
+  render() {
+    const { token } = this.props;
+    return (
+      <Nav>
+        <NavItem className="navbar">
+          <Link
+            to={{
+              pathname: "/search",
+              state: {
+                token: token
+              }
+            }}
+          >
+            Search Genre
+          </Link>
+        </NavItem>
+        <NavItem className="navbar">
+          <Link
+            to={{
+              pathname: "/player",
+              state: {
+                token: token
+              }
+            }}
+          >
+            Spotify Web Player
+          </Link>
+        </NavItem>
+        <NavItem className="navbar">
+          <Link
+            to={{
+              pathname: "/nowplaying",
+              state: {
+                token: token
+              }
+            }}
+          >
+            Analyse Current Track
+          </Link>
+        </NavItem>
+      </Nav>
+    );
+  }
+}
+
+export default NavBar;
